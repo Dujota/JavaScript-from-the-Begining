63da8283e7393b2e94ec782ad5a05215b48d7bd8
Denis Sanchez Dujota <e_scriba@hotmail.com>
2018-04-06 14:42:17 -0400

    playing arpund with data types for both primitive and reference

--- js_sandbox/app.js
+++ js_sandbox/app.js
@@ -38,12 +38,12 @@ console.timeEnd('hello');
 
 // var, let, const
 
-var name = 'John Doe';
-console.log(name);
+var names = 'John Doe';
+console.log(names);
 
 //reassign variable values
-name = 'Steve Smith';
-console.log(name);
+names = 'Steve Smith';
+console.log(names);
 
 // initialize variables
 
@@ -89,3 +89,49 @@ console.log(numbers);
 // we cannot do numbers = somehting new  or person = {new object}
 
 
+//Primitive DataTypes
+
+// String
+const name = 'John Doe';
+console.log(typeof name);
+
+
+// Number
+const age = 45;
+console.log(typeof age);
+
+// Boolean
+const hasKids = true;
+console.log(typeof hasKids);
+
+ // Null
+ const car = null;
+ console.log(typeof car); // returns object but it is a bug , can check stack overflow 
+
+ // Undefined
+
+ let test; // has to be let so we dont get error with const 
+ console.log(typeof test);
+
+ // Symbol
+
+ const sym = Symbol();
+ console.log(typeof sym);
+ 
+ // REFERENCE TYOES - Objects
+
+ //Array
+ const hobbies = ['movies', 'music'];
+ console.log(typeof hobbies);
+
+ // Object Literal
+const address = {
+  city: 'Boston',
+  state: 'MA',
+} 
+console.log(typeof address);
+
+// Dates
+const today = new Date();
+console.log(today);
+console.log(typeof today);
