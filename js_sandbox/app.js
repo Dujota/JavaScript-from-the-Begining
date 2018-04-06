6eceb0ce083ea2d806c0a12b3dd2cc616e2e7ac5
Denis Sanchez Dujota <e_scriba@hotmail.com>
2018-04-06 13:51:30 -0400

    playing around with moret console logs

--- js_sandbox/app.js
+++ js_sandbox/app.js
@@ -16,6 +16,21 @@ console.log({a:1, b:2});
 
 console.table({a:1, b:2})
 
+console.error('this is some error');
+
+console.clear();
+
+console.warn('this is a warning');
+
+//if oyu want to see how long an operation takes, you put it between time and timeEnd
+console.time('hello');
+console.log('how long does this take?');
+console.log('how long does this take?');
+console.log('how long does this take?');
+console.log('how long does this take?');
+console.log('how long does this take?');
+console.log('how long does this take?');
+console.timeEnd('hello');
 
 
 
