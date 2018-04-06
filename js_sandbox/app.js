a1ad9815363fc43263cf8cd4ea7051ca3184bdcb
Denis Sanchez Dujota <e_scriba@hotmail.com>
2018-04-06 13:47:55 -0400

    playing around with different console logs

---
+++ js_sandbox/app.js
@@ -0,0 +1,21 @@
+// one line comments
+
+/*
+multi 
+line 
+comments
+*/
+
+console.log('Hello World');
+console.log(123);
+console.log(true);
+var greetings = "hello";
+console.log(greetings);
+console.log([1,2,3,4]);
+console.log({a:1, b:2});
+
+console.table({a:1, b:2})
+
+
+
+
