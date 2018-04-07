6bc069bf60c3f6df7d31d51c8361b5a3dac50eb0
Denis Sanchez Dujota <e_scriba@hotmail.com>
2018-04-07 01:16:59 -0400

    split replace slice and other string manipulation techniques

--- js_sandbox/app.js
+++ js_sandbox/app.js
@@ -246,6 +246,9 @@ console.log(val);
 const firstNames = 'Willaim';
 const lastNames = 'Johnson';
 const ages = 36;
+const str = 'Hello there my name is Brad'
+const tags = 'web design, web development, programming'
+
 // initialize a new variable to log the results
 let valName;
 
@@ -297,9 +300,31 @@ valName = firstNames.lastIndexOf('l'); // 3 which is position 3
 
 valName = firstNames.charAt(2); // returns l since l is at string[2]
 
-// Get last Char
+// Get last Char *********VERY COMMON************
 valName = firstNames.charAt(firstNames.length - 1) // this gets the last character of a string, this is a very common formula in JS wen dealing with strings
 
+// substring()
+
+valName = firstNames.substring(0, 4); // Will -- takes two arguments, starting point and ending position and will return everything from those two points
+
+// slice() *********VERY COMMON************  mostly used with arrays to pull things out of arrays
+
+valName = firstNames.slice(0, 4); // same as the substring
+
+valName = firstNames.slice(-3); // returns iam, so it starts from the end and works backwords x# of characters 
+
+// split() *********VERY COMMON************   Split strings into arrays based on a separator 
+ valName = str.split(' '); // split at the space and create the following array: ["Hello", "there", "my", "name", "is", "Brad"] where each word is each index. 
+
+ // works great if you have a form on a website with comma separated tags
+valName = tags.split(','); // ["web design", " web development", " programming"]
+
+// replace()
+
+valName = str.replace('Brad', 'Jack'); // Hello there my name is Jack ... it replaces the first argument with the second argument in the replace() function
+
+// includes()
+valName = str.includes('Hello'); // returns boolean after it checks if the passed in string is inside the string being called on
 
 
 
