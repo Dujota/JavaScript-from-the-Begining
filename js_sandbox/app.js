280a03024b7a10be9dddbf04e065a24029b6ac07
Denis Sanchez Dujota <e_scriba@hotmail.com>
2018-04-06 23:56:48 -0400

    more with math object, including how to generate a random number in hjs

--- js_sandbox/app.js
+++ js_sandbox/app.js
@@ -200,7 +200,29 @@ const num2 = 50
 let val;
 
 // Simple Math with numbers 
+val = num1 + num2; // 150
+val = num1 * num2; // 1500
+val = num1 - num2; // 50
+val = num1 / num2; // 2
+val = num1 % num2; // 0 modulus = remainder 
 
+// Math Object -- means it has properties and methods 
+val = Math.PI; //PI is a methods  returns pi
+val = Math.E; // eulers 
+val = Math.round(2.4); // 2
+
+//rounding with Math
+val = Math.ceil(2.4); // 3  rounds up 
+val = Math.floor(2.4); // 2 rounds down
+
+// square 
+val = Math.sqrt(64); // 8
+
+//power of multiplier
+val = Math.pow(8, 2); // 64, we write the 1st number then the power exponent second
+
+// absolute
+val = Math.abs(-3); // 3 abosulte will always give postive #
 
 console.log(val);
 
