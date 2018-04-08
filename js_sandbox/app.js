ee021723b9967c2d4b8621d1625e248752cefa6b
Denis Sanchez Dujota <e_scriba@hotmail.com>
2018-04-08 14:35:45 -0400

    playing aroind with window alerts, prompts and confirm messages

---
+++ js_sandbox/app.js
@@ -0,0 +1,23 @@
+// WINDOW METHODS  / OBJECTS / PROPERTIES
+
+// Alert
+// alert('helloWorld')
+
+
+// Prompt, similar to alert, but it takes an input
+
+/*
+ const input = prompt();
+
+alert(input); //  an alert pops up with a text box
+*/
+
+//  Confirm - used often when you are deleting somehting to make sure they intend to do as they want
+
+/*
+if (confirm('Are you sure')) {  // pops up ok/cancel = true/false 
+  console.log('Yes'); // executes when you hit ok
+} else {
+  console.log('No'); // executes when you hit cancel
+}
+*/
