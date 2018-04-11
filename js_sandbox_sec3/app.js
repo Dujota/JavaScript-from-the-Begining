5ef301a59e71e179deecb624cfffa2ea5b53e68e
Denis Sanchez Dujota <e_scriba@hotmail.com>
2018-04-11 01:09:40 -0400

    started playing around with multiple elements selectors

---
+++ js_sandbox_sec3/app.js
@@ -0,0 +1,6 @@
+// DOM SELECTORS FOR MULTIPLE ELEMENTS
+// they return a node list that behaves like an array but need to be converted to arrays in order to use array methods against them 
+
+
+//document.getElementsByClassName
+
