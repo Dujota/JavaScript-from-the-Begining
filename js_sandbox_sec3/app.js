d50648236b09bba98e984ad927d5d888e09ee16c
Denis Sanchez Dujota <e_scriba@hotmail.com>
2018-04-11 17:27:00 -0400

    playing arpound with query selectoer all and how to use array methods on that

--- js_sandbox_sec3/app.js
+++ js_sandbox_sec3/app.js
@@ -4,7 +4,7 @@
 
 //document.getElementsByClassName
 
-let items = document.getElementsByClassName('collection-item'); // gets all the elements with the classname of collection-item and returns a node list that we can call properties on
+let items = document.getElementsByClassName('collection-item'); // gets all the elements with the classname of collection-item and returns a HTML COLLECTION list that we can call properties on
 console.log(items); // displays the html collection, much like array and if logged we can see the attributes which we can access
 
 console.log(items[0]);
@@ -36,12 +36,45 @@ lis.forEach(function (li, index) {
   console.log(li.className);
 
   li.textContent = `${index} : Can change text after converted`
-
-  
-  
 });
 
 console.log(lis);
+
+console.clear();
+
+// document.querySelectorAll();  can od a for each and other array methods without converting
+
+//can use any css selector 
+const allItems = document.querySelectorAll('ul.collection li.collection-item');
+
+
+//can do array mehtod on the node list 
+allItems.forEach(function(item, index) {
+  item.textContent = `${index}: Hello, i changed this with query selector All`
+});
+
+console.log(allItems);
+
+//select odd or even, can use any psueudo selector 
+const liOdd = document.querySelectorAll('li:nth-child(odd)');
+const liEven = document.querySelectorAll('li:nth-child(even)');
+
+
+//using a for each
+liOdd.forEach(function(li, index) {
+  li.style.background = 'blue'
+});
+
+//using a for loop
+for (let i = 0; i < liEven.length; i++) {
+  liEven[i].style.background = 'lightgreen'  
+}
+
+
+
+
+
+
 
 
 
