16380f21e889957a979f2cd1702442b1dc638d32
Denis Sanchez Dujota <e_scriba@hotmail.com>
2018-04-12 22:07:27 -0400

    started playing around with classes and their attributes, adding removing a class, setting or removing its attriutes and then checking if a class has a particular attribute

--- js_sandbox_sec3/app.js
+++ js_sandbox_sec3/app.js
@@ -25,6 +25,7 @@ cardAction.replaceChild(newHeading, oldHeading);
 
 console.log(newHeading);
 
+console.clear();
 
 // REMOVE ELEMENT
 const listItemsLi = document.querySelectorAll('li');
@@ -37,4 +38,38 @@ listItemsLi[0].remove();
 listUl.removeChild(listItemsLi[3])
 
 
-// Cla
+// CLASSES AND ATTRIBUTES
+const firstLi = document.querySelector('#new-item'); // can do first chile li:first-child
+const aLink = firstLi.children[0]
+let values;
+
+
+// Classes
+
+values = aLink.className //  gives us all the classes assigned to that variable 
+values = aLink.classList // gives a class list which behaves like an array and we can do array mehtods on it
+
+values = aLink.classList[0];
+aLink.classList.add('test'); // can add a class to an element for toggling 
+aLink.classList.remove('test'); // can remove class for toggling
+values = aLink;
+
+// Attributes
+
+values = aLink.getAttribute('href'); //  pulls the href link in this case jsut the '#'
+values = aLink.setAttribute('href', 'http://google.com'); // change that to new link ('attribute', 'new value')
+
+values = aLink.hasAttribute('href'); //  check if it has href, if so, returns true
+values = aLink.hasAttribute('ttile'); // false
+
+aLink.setAttribute('title', 'Google') // now we add the attribute of title
+values = aLink.hasAttribute('title'); // now it becmes true
+
+values = aLink // will show the new title attribute we put on it
+
+aLink.removeAttribute('title');
+
+
+console.log(values);
+
+
