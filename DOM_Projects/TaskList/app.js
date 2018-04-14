779fb975b677287a421ce67db6a5affd972d2b60
Denis Sanchez Dujota <e_scriba@hotmail.com>
2018-04-14 09:31:14 -0400

    created the ui variables that we will work with

---
+++ DOM_Projects/TaskList/app.js
@@ -0,0 +1,7 @@
+// DEFINE UI VARS
+
+const form = document.querySelector('#task-form');
+const taskList = document.querySelector('.collection');
+const clearBtn = document.querySelector('.clear-tasks');
+const filter = document.querySelector('#filter');
+const taskInput = document.querySelector('#task');
