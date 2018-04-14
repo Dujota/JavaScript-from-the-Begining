6e58b573eedbca340b57b54bb8f8af862e30ab23
Denis Sanchez Dujota <e_scriba@hotmail.com>
2018-04-14 09:53:56 -0400

    created a function to load all event listeners, then created event listener for submit, which calls addTask function, filled in the addtask function, check if input has a value, then create li, add a class to it, append text node to li that = input, then create the new link element and add html icon then append it to li and append li to ul and clear input

--- DOM_Projects/TaskList/app.js
+++ DOM_Projects/TaskList/app.js
@@ -5,3 +5,51 @@ const taskList = document.querySelector('.collection');
 const clearBtn = document.querySelector('.clear-tasks');
 const filter = document.querySelector('#filter');
 const taskInput = document.querySelector('#task');
+
+// Load all event listeners
+loadEventListeners();
+
+// Load all event listeners
+
+function loadEventListeners() {
+  // add Task event
+  form.addEventListener('submit', addTask);
+}
+
+
+// ADD A TASK FUNCTION
+function addTask(e) {
+  if (taskInput.value === '') { // check if new task form is empty
+    alert('Add a Task') 
+  } 
+
+  // Create li Element
+  const li = document.createElement('li');
+  
+  // Add class
+  li.className = 'collection-item'; // materialize class (ul use collection and li use collection-item)
+
+  // Create Text node and Append to li
+  li.appendChild(document.createTextNode(taskInput.value));
+  
+  // Create new link element
+  const link = document.createElement('a');
+  // Add Class
+  link.className = 'delete-item secondary-content' // materialize class, secondary content will float to the right on a list item
+
+  // Add icon HTML
+  link.innerHTML = '<i class="fa fa-remove"></i>';
+
+  // Append Link to li
+  li.appendChild(link);
+  
+  // Append li to ul
+  taskList.appendChild(li);
+
+  // Clear the input
+  taskInput.value = '';
+
+  // stop the form submit
+  e.preventDefault();
+}
+
