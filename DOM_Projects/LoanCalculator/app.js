eca361fb896bd3f510536ada7d4d011720bff12f
Denis Sanchez Dujota <e_scriba@hotmail.com>
2018-04-15 13:19:16 -0400

    worked through the logic of the calculator and implemented the logic, 1st by grabbing allthe variables we need, then working with the calculations themselves, then use an if to check if the monthly came back ok, if so, then set the values of the display inputs to their respective fields

--- DOM_Projects/LoanCalculator/app.js
+++ DOM_Projects/LoanCalculator/app.js
@@ -2,6 +2,34 @@
 
 document.querySelector('#loan-form').addEventListener('submit', calculateResults);
 
-function calculateResults(params) {
+function calculateResults(e) {
+  console.log('Calculting....'); // test out the stop default
   
+  // UI Variables
+  const amount = document.getElementById('amount');
+  const interest = document.getElementById('interest');
+  const years = document.getElementById('years');
+  const monthlyPayment = document.getElementById('monthly-payment');
+  const totalPayment = document.getElementById('total-payment');
+  const totalInterest = document.getElementById('total-interest');
+
+  // Calculations stored in Variables
+  const principal = parseFloat(amount.value); // parsing the value of amount field into a float
+  const calculatedInterest = parseFloat(interest.value) / 100 / 12;
+  const calculatedPayments = parseFloat(years.value) * 12;
+  
+  // Compute monthly payment
+  const x = Math.pow(1 + calculatedInterest, calculatedPayments);
+  const monthly = (principal * x * calculatedInterest)/(x - 1)
+
+  if (isFinite(monthly)) {
+    monthlyPayment.value = monthly.toFixed(2); // sets the # have a fixed # of decimals
+    totalPayment.value = (monthly * calculatedPayments).toFixed(2);
+    totalInterest.value = ((monthly * calculatedPayments) - principal).toFixed(2);
+  } else {
+    console.log('Please check your numbers');
+    
+  }
+ e.preventDefault();
 }
+
