ba6c0add4c557b987e94dcb4eeb01f8e78cb1a31
Denis Sanchez Dujota <e_scriba@hotmail.com>
2018-04-17 12:38:47 -0400

    started playing around with inheritance/subclasses in ES6

---
+++ js_sandbox_sec5/app.js
@@ -0,0 +1,24 @@
+// ES6 Inheritance or aka SubClass
+
+// we can create a class and then extend (subclass) 
+
+class Person {
+  constructor(firstName, lastName) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  greeting(){
+    return `Hello there ${this.firstName} ${this.lastName}`
+  }
+}
+
+
+// syntax is class NewClass extends InheritanceClass, Customer is subclass of Person
+class Customer extends Person {
+  constructor(firstName, lastName, phone, membership) {
+    super(firstName, lastName); // calls the parent class constructor and we pass in whatever the argumetns that we will inherit from the parent class
+    this.phone = phone;
+    this.membership = membership;
+  }
+}
