d434cc54c1c2c88819970c11215c8c34738c66f0
Denis Sanchez Dujota <e_scriba@hotmail.com>
2018-05-04 14:24:08 -0400

    created the fucnctionality to append each customer for each time we loop through the array

--- js_sandbox_sec7/app.js
+++ js_sandbox_sec7/app.js
@@ -1,5 +1,13 @@
+// Single Customer
 document.getElementById('button1').addEventListener('click', loadCustomer);
 
+//Array of customers
+document.getElementById('button2').addEventListener('click', loadCustomers);
+
+
+//METHODS---------------
+
+// Load Single Customer
 function loadCustomer(e) {
  const xhr = new XMLHttpRequest();
 
@@ -27,3 +35,35 @@ function loadCustomer(e) {
   //Send
   xhr.send();
 }
+
+// Load Array of  Customers
+function loadCustomers(e) {
+  const xhr = new XMLHttpRequest();
+ 
+   //Open
+   xhr.open('GET', 'customers.json', true); // this works almost the same as if we were pulling from an api 
+ 
+   // OnLoad
+   xhr.onload = function () {
+     if(this.status === 200){
+       // console.log(this.responseText);
+       const customers = JSON.parse(this.responseText) // we do this because this is a jason string so we need to run it through the parse to get an object
+      
+       let output = ''; //  set empty variable outside loop with let since we will mutate the value and then we append += each iteration to the output variable so that they all save, otherwise it will overide without the += 
+       customers.forEach(function(customer) {
+        output += ` 
+        <ul>
+        <li>ID: ${customer.id}</li>
+        <li> Name: ${customer.name}</li>
+        <li>Company: ${customer.company}</li>
+        <li>Phone: ${customer.phone}</li>
+        </ul>
+        `
+       });
+       document.getElementById('customers').innerHTML = output;
+     }
+   }
+ 
+   //Send
+   xhr.send();
+ }
