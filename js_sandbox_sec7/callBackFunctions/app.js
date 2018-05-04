2b932efabcaa1ef6f6765ba8d68b0c638fe31023
Denis Sanchez Dujota <e_scriba@hotmail.com>
2018-05-04 18:57:02 -0400

    created scaffold of the ui and then did two functions that will mimic the server response without a callback

---
+++ js_sandbox_sec7/callBackFunctions/app.js
@@ -0,0 +1,21 @@
+const posts = [
+  {title: 'Post One', body: 'This is post one'},
+  {title: 'Post One', body: 'This is post one'}
+];
+
+function createPost(post) {
+  setTimeout(function() {
+    posts.push(post);
+  }, 2000);
+}
+
+function getPost() {
+  setTimeout(function () {
+    let output = '';
+    posts.forEach(function(post) {
+      output += `<li>${post.title}</li>`
+    });
+
+    document.body.innerHTML = output;
+  },1000);
+}
