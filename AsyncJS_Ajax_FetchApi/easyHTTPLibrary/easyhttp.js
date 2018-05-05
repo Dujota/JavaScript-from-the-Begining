0b1f6e467c52b846346bf149cd0bbe8756e19138
Denis Sanchez Dujota <e_scriba@hotmail.com>
2018-05-05 12:43:47 -0400

    created the html and starting to fill in the http class using es5

---
+++ AsyncJS_Ajax_FetchApi/easyHTTPLibrary/easyhttp.js
@@ -0,0 +1,13 @@
+function easyHTTP() {
+  this.http = XMLHttpRequest();
+}
+
+// Prototype Methods for this class we are constructing
+
+// Make an HTTP GET Request
+
+// Make an HTTP PUT Request
+
+// Make an HTTP POST Request
+
+// Make an HTTP DELETE Request
