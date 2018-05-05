c3a511f932fc2c3974d312a77a1ab872cc45f571
Denis Sanchez Dujota <e_scriba@hotmail.com>
2018-05-05 13:13:48 -0400

    created a simple request with a callback and then invokled the callback inside the onload check after we binded the this keyword to self so we can use inside the function scope then added an error and displayed the status of the error if it is caught and changed the use of the function by eitehr displaying the error in the log or the response in the log

--- AsyncJS_Ajax_FetchApi/easyHTTPLibrary/app.js
+++ AsyncJS_Ajax_FetchApi/easyHTTPLibrary/app.js
@@ -1,4 +1,11 @@
 const http = new easyHTTP;
 
 // Get Posts 
-http.get('https://jsonplaceholder.typicode.com/posts');
+http.get('https://jsonplaceholder.typicode.com/posts', function (error, posts) { // since we added an error to the callback, then we need to add in the parameter here or else it will display null first, then we test for the error. 
+  if (error) { // so it will check if there is an error that is not null by passing condition and then displayt that in the log instead of the response, else the response 
+    console.log(error);
+    
+  } else {
+    console.log(posts); 
+  }
+});   // here we pass in a callback to get the posts while the page is loading so that we dont get undefined, we need this inside the library prortotype function so we can execute a callback when passing in parameters 
--- AsyncJS_Ajax_FetchApi/easyHTTPLibrary/easyhttp.js
+++ AsyncJS_Ajax_FetchApi/easyHTTPLibrary/easyhttp.js
@@ -5,7 +5,7 @@ function easyHTTP() {
 // Prototype Methods for this class we are constructing
 
 // Make an HTTP GET Request
-easyHTTP.prototype.get = function (url) {
+easyHTTP.prototype.get = function (url, callback) { // we need a callback to return our response otherwise we will get undefined if we try to log it on a return statement, this is because oif syncronous responses 
   // Open
   this.http.open('GET', url, true);
 
@@ -13,8 +13,9 @@ easyHTTP.prototype.get = function (url) {
   let self = this; // a fix for the problem listed below which will capture the scope of 'this' and we can use that scope inside the function. 
   this.http.onload = function () {
     if (self.http.status === 200) { // this.http.status is undefined here inside the funciton cuz 'this' scopes to the function itself not the parent, ES6 arrow function fixes this issue
-      console.log(self.http.responseText);
-      
+      callback(null, self.http.responseText); // insted of just sending back response, we want to send back an error if there is one, like nodeJS
+    } else {
+      callback('Error: ' + self.http.status); // if there is an error, it will display that code through the error
     }
   }
 
