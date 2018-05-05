44206f01c25043e0de50234d8658757ca653dbf1
Denis Sanchez Dujota <e_scriba@hotmail.com>
2018-05-05 12:48:57 -0400

    started the get request response and testing the status with a console log

--- AsyncJS_Ajax_FetchApi/easyHTTPLibrary/easyhttp.js
+++ AsyncJS_Ajax_FetchApi/easyHTTPLibrary/easyhttp.js
@@ -5,9 +5,21 @@ function easyHTTP() {
 // Prototype Methods for this class we are constructing
 
 // Make an HTTP GET Request
+easyHTTP.prototype.get = function (url) {
+  this.http.open('GET', url, true);
+
+  this.http.onload = function () {
+    if (this.http.status === 200) {
+      console.log(this.http.responeText);      
+    }
+  }
+
+  this.http.send();
+}
 
 // Make an HTTP PUT Request
 
 // Make an HTTP POST Request
 
 // Make an HTTP DELETE Request
+
