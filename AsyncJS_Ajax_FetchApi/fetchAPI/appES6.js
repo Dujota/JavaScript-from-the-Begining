10bd5a967040a44fba36ea7a6931c541d0ec4ca2
Denis Sanchez Dujota <e_scriba@hotmail.com>
2018-05-07 01:28:28 -0400

    created a duplicate file for the fetch api example and converted all of it to the ES6 syntax

---
+++ AsyncJS_Ajax_FetchApi/fetchAPI/appES6.js
@@ -0,0 +1,44 @@
+
+ 
+//fetch API returns a promise so we use the .then(function () {do somehting}) syntax and run a function after we get the file
+
+// GET LOCAL TEXT FILE DATA
+
+document.getElementById('button1').addEventListener('click', getText);
+
+function getText () { 
+  fetch('test.txt')
+  .then(response => response.text())  
+  .then( data => { document.getElementById('output').innerHTML = data;})
+  .catch(error => console.log(error));
+}
+
+
+// GET LOCAL JSON DATA 
+document.getElementById('button2').addEventListener('click', getJSON);
+
+function getJSON () {
+  fetch('posts.json')
+  .then ( response => response.json())
+  .then(data => {
+    let output = ''
+    data.forEach( post => output += `<li>${post.title}</li>`)
+    document.getElementById('output').innerHTML = output; 
+  })
+  .catch(error => console.log(error));
+}
+
+// Get Data from API
+
+document.getElementById('button3').addEventListener('click', getExternalApi);
+
+function getExternalApi () {
+  fetch('https://api.github.com/users')
+  .then(response => response.json())
+  .then(data => {
+    let output = '';
+    data.forEach(user => output += `<li>${user.login}</li>`);
+    document.getElementById('output').innerHTML = output;
+  })
+  .catch(error => console.log(error));
+}
