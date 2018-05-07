54f60cf0a9c2ede8d5d58ebaad1b1f74bb07ee73
Denis Sanchez Dujota <e_scriba@hotmail.com>
2018-05-06 23:59:30 -0400

    finished the promises practice by mimicing a server error and catching the error with the .catch(function(){}) syntax at the end of the async call and the logged it in the console, now looking at the fetch API

--- AsyncJS_Ajax_FetchApi/promises/app.js
+++ AsyncJS_Ajax_FetchApi/promises/app.js
@@ -9,7 +9,16 @@ function createPost(post) {
     // we move the setTimeout function inside the promise, and instead of a callback we invoke the resolve where the callback would have been
     setTimeout(function () {
       posts.push(post);
-      resolve(); // invoke the promise where the callback would have been 
+
+      const error = false; // mimic an error on the server
+
+      if (!error) { // if no error then invoke the resolve 
+        resolve();
+      } else { // else show the error in condole
+        reject('Error, Something went Wrong!');
+      }
+
+      // resolve(); // invoke the promise where the callback would have been 
     }, 2000);
   });
 }
@@ -27,6 +36,11 @@ function getPosts() {
 createPost({
   title: 'Post Three',
   body: 'This is the third post'
-}).then(getPosts); // instead of passing in a function for the callback we use the .then syntax at the end which will take the function we want to run after we are done with the create post. essentially it will run getPosts after the set timeout 
+})
+.then(getPosts)// instead of passing in a function for the callback we use the .then syntax at the end which will take the function we want to run after we are done with the create post. essentially it will run getPosts after the set timeout
+.catch(function (error) {
+  console.log(error);
+  
+}); 
 
 getPosts();
