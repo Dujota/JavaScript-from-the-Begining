424ba85f081c76972d60d8e2676bd5153fabe2f6
Denis Sanchez Dujota <e_scriba@hotmail.com>
2018-05-06 23:49:40 -0400

    played around with a pormise, made it work just like the async with callbacks

--- AsyncJS_Ajax_FetchApi/promises/app.js
+++ AsyncJS_Ajax_FetchApi/promises/app.js
@@ -3,11 +3,18 @@ const posts = [
   {title: 'Post Two', body: 'This is post one'}
 ];
 
-function createPost(post, callback) {
-  setTimeout(function () {
-    posts.push(post);
-    callback();
-  }, 2000);
+function createPost(post) {
+  return new Promise(function (resolve, reject) { // construct the new promise, pass in a function that takes two arguments, resolve is what to do when async done, reject is for any error handling. 
+
+    // we move the setTimeout function inside the promise, and instead of a callback we invoke the resolve where the callback would have been
+    setTimeout(function () {
+      posts.push(post);
+      resolve(); // invoke the promise where the callback would have been 
+    }, 2000);
+    
+  });
+
+
 }
 
 function getPosts() {
@@ -23,6 +30,6 @@ function getPosts() {
 createPost({
   title: 'Post Three',
   body: 'This is the third post'
-}, getPosts);
+}).then(getPosts); // instead of passing in a function for the callback we use the .then syntax at the end which will take the function we want to run after we are done with the create post. essentially it will run getPosts after the set timeout 
 
 getPosts();
