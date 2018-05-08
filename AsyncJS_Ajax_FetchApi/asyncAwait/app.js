e81566a053a425df2d1cf5088a744b835e5da9e0
Denis Sanchez Dujota <e_scriba@hotmail.com>
2018-05-07 23:26:27 -0400

    playing aroudn with catching errors in new promise with await

--- AsyncJS_Ajax_FetchApi/asyncAwait/app.js
+++ AsyncJS_Ajax_FetchApi/asyncAwait/app.js
@@ -5,11 +5,18 @@ async function myFunc () {
     setTimeout(() => resolve('Hello in Timeout'), 1000);
   });
 
-  const response = await promise; // wait until promise is resolved 
+  const error = true // fabricating an error
+  if (!error) {
+    const response = await promise; // wait until promise is resolved 
 
-  return response // funciton will return promise value 
+    return response // funciton will return promise value   
+  } else {
+    await Promise.reject(new Error('Something went wrong!')) // we have to use the class method as we are outside the promise and then call reject method, which will then need an error message passed into it
+  }
+  
 }
 
 myFunc()
-.then(response => console.log(response));
+.then(response => console.log(response))
+.catch(error => console.log(error));
 
