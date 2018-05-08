2d4b1fbd709981a0c99e8b630f9763066c28c120
Denis Sanchez Dujota <e_scriba@hotmail.com>
2018-05-07 23:28:10 -0400

    moved the example to its own file and will now incorporate the async await to the fetch api library neing built

--- AsyncJS_Ajax_FetchApi/asyncAwait/app.js
+++ AsyncJS_Ajax_FetchApi/asyncAwait/app.js
@@ -1,22 +1,2 @@
-async function myFunc () {
 
-  // create a new promise insde the async await promise
-  const promise = new Promise((resolve, reject) => { // 
-    setTimeout(() => resolve('Hello in Timeout'), 1000);
-  });
-
-  const error = true // fabricating an error
-  if (!error) {
-    const response = await promise; // wait until promise is resolved 
-
-    return response // funciton will return promise value   
-  } else {
-    await Promise.reject(new Error('Something went wrong!')) // we have to use the class method as we are outside the promise and then call reject method, which will then need an error message passed into it
-  }
-  
-}
-
-myFunc()
-.then(response => console.log(response))
-.catch(error => console.log(error));
 
