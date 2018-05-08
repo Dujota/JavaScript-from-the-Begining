fa25c5dc9dff09f8233464c631761d7a68a66cda
Denis Sanchez Dujota <e_scriba@hotmail.com>
2018-05-08 14:14:56 -0400

    created the ui for the api to display, which is an avatar, the url to see the github profile, then a list of some items that are on the user profile from github

--- gitHubFinder/ui.js
+++ gitHubFinder/ui.js
@@ -4,6 +4,32 @@ class UI {
   }
 
   showProfile(user) {
-    console.log(user);
+    // console.log(user);
+    this.profile.innerHTML = `
+      <div class="card card-body mb-3">
+        <div class="row">
+          <div class="col-md-3">
+            <img class="img-fluid mb-2" src="${user.avatar_url}">
+            <a href="${user.html_url}" target="_blank" class = "btn btn-primary btn-block">View Prfile</a>
+          </div>
+
+          <div class="col-md-9">
+            <span class="badge badge-primary">Public Repos: ${user.public_repos}</span>
+            <span class="badge badge-secondary">Public Gists: ${user.public_gists}</span>
+            <span class="badge badge-success">Public Followers: ${user.followers}</span>
+            <span class="badge badge-info">Public Following: ${user.following}</span>
+            <br><br>
+            <ul class="list-group">
+              <li class="list-group-item">Company: ${user.company}</li>
+              <li class="list-group-item">Website/Blog: ${user.blog}</li>
+              <li class="list-group-item">Location: ${user.location}</li>
+              <li class="list-group-item">Member Since: ${user.created_at}</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <h3 class="page-heading mb-3>Latest Repos</h3>
+      <div id="repos"></div>
+    `;
   }
 }
