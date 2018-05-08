818636b14851fbe1f01f9d35c97480571d96ef06
Denis Sanchez Dujota <e_scriba@hotmail.com>
2018-05-08 00:49:20 -0400

    filled in the if statement to check if there is a not found message for the profile data and scaffolded some outcomes to fill in now

--- gitHubFinder/app.js
+++ gitHubFinder/app.js
@@ -1,3 +1,7 @@
+// Init
+const github = new GitHub;
+
+
 // Search Input
 
 const searchUser = document.getElementById('searchUser');
@@ -7,7 +11,19 @@ searchUser.addEventListener('keyup', (e) => {
   // Get input Text
   const userText = e.target.value
   if (userText !== '') {
-    console.log(userText);
+    // Make HTTP Call
+    github.getUser(userText)
+    .then (data => {
+      if (data.profile.message === 'Not Found') {
+        // Show Alert that the user is not found
+      } else {
+        // Show the profile
+
+      }
+      
+    })
     
+  } else {
+    // Clear the profile
   } 
 });
