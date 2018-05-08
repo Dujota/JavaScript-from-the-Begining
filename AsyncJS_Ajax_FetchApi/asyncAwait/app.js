f6bac5f00c508a22d8d478edea284a8088763c32
Denis Sanchez Dujota <e_scriba@hotmail.com>
2018-05-07 23:20:21 -0400

    playing around with async await and creating promises inside the async await to trigger when a response will be executed by the function

--- AsyncJS_Ajax_FetchApi/asyncAwait/app.js
+++ AsyncJS_Ajax_FetchApi/asyncAwait/app.js
@@ -1,3 +1,15 @@
-function myFunc () {
-  // body
+async function myFunc () {
+
+  // create a new promise insde the async await promise
+  const promise = new Promise((resolve, reject) => { // 
+    setTimeout(() => resolve('Hello in Timeout'), 1000);
+  });
+
+  const response = await promise; // wait until promise is resolved 
+
+  return response // funciton will return promise value 
 }
+
+myFunc()
+.then(response => console.log(response));
+
