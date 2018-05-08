4f8da830a0bdc9ab7b74fc8031c3f75bc9404f99
Denis Sanchez Dujota <e_scriba@hotmail.com>
2018-05-07 23:32:51 -0400

    did the getUsers call with async await

--- AsyncJS_Ajax_FetchApi/asyncAwait/app.js
+++ AsyncJS_Ajax_FetchApi/asyncAwait/app.js
@@ -1,2 +1,17 @@
+// normally we would use the .then() syntax but with async await we dont need to
 
+async function getUsers () {
 
+  // await response of the fetch call
+  const response = await fetch('https://jsonplaceholder.typicode.com/users'); 
+
+  // We only proceed once the call is resolved
+  const data = await response.json();
+
+  // Only proceed once the second promise is resolved
+  return data;
+}
+
+// now we call the .then on the promise made by the async function
+getUsers()
+  .then(users => console.log(users));
