6fe13797962281f551c636c9c6b3ebf87632318f
Denis Sanchez Dujota <e_scriba@hotmail.com>
2018-05-08 15:26:05 -0400

    implemented the ui that pushes the repos from the array returned in the object saved by the fetch call

--- gitHubFinder/github.js
+++ gitHubFinder/github.js
@@ -10,7 +10,7 @@ class GitHub {
   async getUser(user) {
     const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
 
-    const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
+    const reposResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
 
     const profile = await profileResponse.json();
     const repos = await reposResponse.json();
--- gitHubFinder/ui.js
+++ gitHubFinder/ui.js
@@ -29,9 +29,33 @@ class UI {
           </div>
         </div>
       </div>
-      <h3 class="page-heading mb-3>Latest Repos</h3>
+      <h3 class="page-heading mb-3">Latest Repos</h3>
       <div id="repos"></div>
     `;
+  }
+
+  // Show the user Repos
+  showRepos(repos){
+    let output = '';
+    repos.forEach(function(repo) {
+      output += `
+        <div class="card card-body mb-2">
+          <div class="row">
+            <div class="col-md-6">
+              <a href="${repo.html_url}" target="_blank">${repo.name}</a>
+            </div>
+            <div class="col-md-6">
+            <span class="badge badge-primary">Stars: ${repo.stargazers_count}</span>
+            <span class="badge badge-secondary">Watchers: ${repo.watchers_count}</span>
+            <span class="badge badge-success">Forks: ${repo.forks_count}</span>
+            </div>
+          </div>  
+        </div>
+      `;
+    });
+
+    // Output repos
+    document.getElementById('repos').innerHTML = output;
   }
 
   // Clear the UI when the input is emplty
