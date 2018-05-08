4420efa55990a90b10874d40a997b43db621d62a
Denis Sanchez Dujota <e_scriba@hotmail.com>
2018-05-07 23:50:41 -0400

    filled in the remaining methods for the http library, all works with sync await

--- AsyncJS_Ajax_FetchApi/easyHTTP3-Library-Async-Await.1/easyhttp3.js
+++ AsyncJS_Ajax_FetchApi/easyHTTP3-Library-Async-Await.1/easyhttp3.js
@@ -10,7 +10,6 @@
 class EasyHTTP {
   // Make an HTTP GET Request
 
- 
   async get(url){
     const response = await fetch('https://jsonplaceholder.typicode.com/users')
     const resData = await response.json();
@@ -19,50 +18,42 @@ class EasyHTTP {
 
   // Make an HTTP Post Request
 
-  // for the post we need to add an object with some identifiers for the data and what we are actually trying to do, similar to how xhr needs to define the header, the object has a special format that can be obseved below. so post takes two parameters url & header object
-  post(url, data){
-    return new Promise((resolve, reject) =>{
-      fetch(url, {
+  async post(url, data){
+      const response = await fetch(url, {
         method: 'POST',
         headers: {
           'Content-type': 'application/json'
         },
         body: JSON.stringify(data)
-      })
-      .then(response => response.json())
-      .then(data => resolve(data))
-      .catch(error => reject(error));
-    });
+      });
+
+      const resData = await response.json();
+      return resData
   }
 
   // Make an HTTP PUT Request
-  put(url, data) {
-    return new Promise((resolve, reject) => {
-      fetch(url, {
+  async put(url, data) {
+      const response = await fetch(url, {
         method: 'PUT',
         headers: {
           'Content-type': 'application/json'
         },
         body: JSON.stringify(data)
-      })
-      .then(response => response.json())
-      .then(data => resolve(data))
-      .catch(error => reject(error))
-    });
+      });
+
+      const resData = await response.json();
+      return resData;
   }
 
-  delete(url) {
-    return new Promise((resolve, reject) => {
-      fetch(url, {
+  async delete(url) {
+      const response = await fetch(url, {
         mehtod: 'DELETE',
         headers: {
           'Content-type': 'application/json'
         } // removed the body since we are deleting
-      })
-      .then(response => response.json())
-      .then(() => resolve('Resource deleted ... ')) // we dont send back data, instead we send back a string message
-      .catch(error => reject(error))
-    });
+      });
+      const resData = await 'Resource Deleted ... ' // for deleted portion we just return whatever msg we wanted instead of the response.json()
+      return resData
   }
 }
 
