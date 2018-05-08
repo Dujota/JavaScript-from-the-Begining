0fcaec13dc26783be70625bab5ea580a3e96d0ef
Denis Sanchez Dujota <e_scriba@hotmail.com>
2018-05-07 23:35:46 -0400

    copied the http2 library and will reffactor with async await

---
+++ AsyncJS_Ajax_FetchApi/easyHTTP3-Library-Async-Await.1/app.js
@@ -0,0 +1,29 @@
+const http = new EasyHTTP;
+
+// GET Users
+
+http.get('https://jsonplaceholder.typicode.com/users')
+  .then(data => console.log(data))
+  .catch(error => console.log(error));
+
+// USER DATA
+const data = {
+  name: 'John Doe',
+  username: 'johndoe',
+  email: 'jdoe@gmail.com'
+}
+
+// CREATE USER -- will show new user id # 11
+http.post('https://jsonplaceholder.typicode.com/users', data)
+  .then(data => console.log(data))
+  .catch(error => console.log(error));
+
+// PUT -- shows user id 2 was updated with post data
+http.put('https://jsonplaceholder.typicode.com/users/2', data)
+  .then(data => console.log(data))
+  .catch(error => console.log(error));
+
+// Delete USER
+http.delete('https://jsonplaceholder.typicode.com/users/2')
+  .then(data => console.log(data))
+  .catch(error => console.log(error));  
---
+++ AsyncJS_Ajax_FetchApi/easyHTTP3-Library-Async-Await.1/easyhttp2.js
@@ -0,0 +1,75 @@
+/** 
+ EasyHTTP Library
+ Library for making HTTP Requests
+
+ @version 2.0.0 
+ @author Dujota
+ @license MIT
+**/
+
+class EasyHTTP {
+  // Make an HTTP GET Request
+
+  // if we dont wrap the whole fetch in a promise, if we save the response in a variable and try to log it, we will get undefined. 
+  get(url){
+    return new Promise((resolve, reject) => { // promise will always take a resolve and reject
+      fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        resolve(data)
+      })
+      .catch(error => {reject(error)});
+    });
+    
+  }
+
+  // Make an HTTP Post Request
+
+  // for the post we need to add an object with some identifiers for the data and what we are actually trying to do, similar to how xhr needs to define the header, the object has a special format that can be obseved below. so post takes two parameters url & header object
+  post(url, data){
+    return new Promise((resolve, reject) =>{
+      fetch(url, {
+        method: 'POST',
+        headers: {
+          'Content-type': 'application/json'
+        },
+        body: JSON.stringify(data)
+      })
+      .then(response => response.json())
+      .then(data => resolve(data))
+      .catch(error => reject(error));
+    });
+  }
+
+  // Make an HTTP PUT Request
+  put(url, data) {
+    return new Promise((resolve, reject) => {
+      fetch(url, {
+        method: 'PUT',
+        headers: {
+          'Content-type': 'application/json'
+        },
+        body: JSON.stringify(data)
+      })
+      .then(response => response.json())
+      .then(data => resolve(data))
+      .catch(error => reject(error))
+    });
+  }
+
+  delete(url) {
+    return new Promise((resolve, reject) => {
+      fetch(url, {
+        mehtod: 'DELETE',
+        headers: {
+          'Content-type': 'application/json'
+        } // removed the body since we are deleting
+      })
+      .then(response => response.json())
+      .then(() => resolve('Resource deleted ... ')) // we dont send back data, instead we send back a string message
+      .catch(error => reject(error))
+    });
+  }
+}
+
+
