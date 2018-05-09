5f3fb0c71be0517f5ddaf67f4c89a13e3b4a6483
Denis Sanchez Dujota <e_scriba@hotmail.com>
2018-05-09 02:02:30 -0400

    completed weather class, constructor accepts city and statem change class modifies the attributes, getweather fetches api data

---
+++ WeatherJS/weather.js
@@ -0,0 +1,22 @@
+class Weather () {
+  constructor(city, state) {
+    this.apiKey = '8f45c1e4ff196a01';
+    this.city = city;
+    this.state = state;
+  }
+
+  // Fetch Weather from API
+  async getWeather() {
+    const response = await fetch(`http://api.wunderground.com/api/${this.apiKey}/conditions/q/${this.state}/${this.city}.json`);
+
+    const responseData = await response.json();
+
+    return responseData.current_observation;
+  }
+
+  // Change weather location
+  changeLocation(city, state) {
+    this.city = city;
+    this.state = state;
+  }
+}
