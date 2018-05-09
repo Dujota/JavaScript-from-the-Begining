8de53672a389ae75edc45dc0f812895a7bd34dda
Denis Sanchez Dujota <e_scriba@hotmail.com>
2018-05-09 02:34:17 -0400

    setup the functionality for the change location and closing the modal by grabbing the two elements in the modal input and passing the values to the changeLocation function and fetching the new info, then use jquery to hide modal as bootstrap requires the jquery

--- WeatherJS/app.js
+++ WeatherJS/app.js
@@ -4,11 +4,27 @@ const weather = new Weather('Boston', 'MA');
 // INIT UI Object
 const ui = new UI();
 
+
+// EVENT LISTENERS
+
 // Get Weather when DOM Loaded good practice
 document.addEventListener('DOMContentLoaded', getWeather);
 
+// Change Location Event
+document.getElementById('w-change-btn').addEventListener('click', (e) => {
+  const city = document.getElementById('city').value;
+  const state = document.getElementById('state').value;
 
-// weather.changeLocation('Miami', 'FL');
+  weather.changeLocation(city, state);
+
+  // Get & Display Weather with new Location 
+  getWeather();
+
+  // Close Modal
+  $('#locModal').modal('hide');
+});
+
+
 
 // Moved class function inside anther function so i can call it when the dom is loaded 
 function getWeather (arguments) {
