30e20e5a16d511acf9163d7cd8decbacd317d319
Denis Sanchez Dujota <e_scriba@hotmail.com>
2018-05-09 16:24:08 -0400

    played around with different types of metacharacters to see hwo they impact a matching string:

--- RegEx/app.js
+++ RegEx/app.js
@@ -5,9 +5,19 @@ let re;
 re = /hello/;
 re = /hello/i;
 
+// Meta Character Symbols 
+re = /^h/i; // this means:  must start with ^ = starts  
+re = /d$/i;  // this means: must end with $ = ends
+re = /^hello$/i; // this says it can only be hello, so it has to start and end with hello
+re = /h.llo/i; // this matches any ONE charcater in the place where the . is
+re = /h*llo/i; // this matches any characters 0 or more times in the place where the * is
+re = /gra?e?y/i; // optional character, which will match with either character or neither
+
+re = /gra?e?y\?/i;; //Escape Characters are anything after a forward slash \
+
 
 // String to Match
-const str = 'Hello World';
+const str = 'Gray?';
 
 // Log results
 const result = re.exec(str);
